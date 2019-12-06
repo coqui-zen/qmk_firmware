940208e6901594199bc9a0103e7912d87fc909a7
IanGC <ian@thecommittedbug.io>
2019-12-06 13:27:49 -0500

    Change function call from register() to ta() for encoder behavior

--- keyboards/preonic/keymaps/thehalfdeafchef/keymap.c
+++ keyboards/preonic/keymaps/thehalfdeafchef/keymap.c
@@ -250,11 +250,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
   } else {
     if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
+      tap_code(KC_PGDN);
     } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
+      tap_code(KC_PGUP);
     }
   }
 }
