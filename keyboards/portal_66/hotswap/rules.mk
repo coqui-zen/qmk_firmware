d62e07e57750b60876b63588884b67c051f53e36
Matthew Dias <matthewdias@me.com>
2021-11-15 18:29:19 -0600

    [Keyboard] Add layout options, hotswap version to portal 66 (#14719)
    Co-authored-by: Drashna Jaelre <drashna@live.com>

--- keyboards/portal_66/hotswap/rules.mk
+++ keyboards/portal_66/hotswap/rules.mk
@@ -8,13 +8,22 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+<<<<<<<< HEAD:keyboards/paprikman/albacore/rules.mk
 
-LAYOUTS = 65_ansi_blocker
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+
+LTO_ENABLE = yes
+========
+>>>>>>>> 3794ac4e94 ([Keyboard] Add layout options, hotswap version to portal 66 (#14719)):keyboards/portal_66/hotswap/rules.mk
