ebd86cebec5d1ddd3a518ddbc433e0e4643f2ac1
Ian C <16412897+coquizen@users.noreply.github.com>
2023-06-26 07:18:25 -0400

    minor edit

--- keyboards/planck/keymaps/coquizen/keymap.c
+++ keyboards/planck/keymaps/coquizen/keymap.c
@@ -70,8 +70,7 @@ combo_t key_combos[COMBO_COUNT] = {[SUP_2] = COMBO(sup2_combo, UC(0x00B9)), [SUP
 #define RAISE MO(_RAISE)
 #define TD_LWR TD(A_LOWER)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS]
-                              [MATRIX_COLS] = {
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ┌──────┬──────┬──────┬──────┬────────┬─────┬─────┬───────┬──────┬──────┬────┬──────┐
 //    │ tab  │  q   │  w   │  e   │   r    │  t  │  y  │   u   │  i   │  o   │ p  │ bspc │
 //    ├──────┼──────┼──────┼──────┼────────┼─────┼─────┼───────┼──────┼──────┼────┼──────┤
@@ -85,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS]
   KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T   , KC_Y   , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSPC,
   KC_ESC  , KC_A    , KC_S    , KC_D    , KC_F   , KC_G   , KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
   KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V   , KC_B   , KC_N   , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT ,
-  KC_LCTL , KC_LALT  , KC_LGUI  , KC_ALGR, TD_LWR , KC_SPC , KC_SPC , RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
+  KC_LCTL , KC_LALT , KC_LGUI , KC_ALGR , TD_LWR , KC_SPC , KC_SPC , RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
 ),
 
 //    ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬───────┬──────────┬─────────────┬──────┬─────┐
